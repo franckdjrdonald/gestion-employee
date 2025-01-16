aa8b0d897d8960d5908d5a3558e7754c29b2cb28
RAVOMANANA <jeanaime.ravomanana@gmail.com>
2025-01-16 23:54:31 +0400

    fix: remove unused repository method

--- src/main/java/com/example/gestionemployee/repository/EmployeeRepository.java
+++ src/main/java/com/example/gestionemployee/repository/EmployeeRepository.java
@@ -4,8 +4,6 @@ import com.example.gestionemployee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    boolean existedByName(String name);
 
     boolean existsByName(String name);
-
 }
